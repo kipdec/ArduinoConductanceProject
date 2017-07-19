e69d17f22d7266171b34276d78060f76ef55d87e
kipdec <kip@kips-mbp.uwyo.edu>
2017-07-19 16:25:39 -0600

    Mux interpreter
    This code will serve as the basis for our master code. As of yet
    untested.

--- MuxInterpreter/MuxInterpreterV1.0.cpp
+++ MuxInterpreter/MuxInterpreterV1.0.cpp
@@ -24,14 +24,84 @@ int plantA = 22;
 int plantB = 23;
 int plantC = 24;
 
-// Setup analog reading pin
-int aPin = 0;
+// Set the time to wait between readings
+int wait = 2000;
+
+// setup analog value
+double aPin = 0;
 
 
 void setup(){
 	// Set up the Baud channel which the Arduino will relay data over.
 	Serial.begin(9600);
-	// Prepare pins for output
 
+	// Prepare the digital pins for output to the mux
+	pinMode(pinA, OUTPUT);
+	pinMode(pinB, OUTPUT);
+	pinMode(pinC, OUTPUT);
+	pinMode(pinD, OUTPUT);
 
+	// Prepare the digital pins to power the plants
+	pinMode(plantA, OUTPUT);
+	pinMode(plantB, OUTPUT);
+	pinMode(plantC, OUTPUT);
+
+	// Prepare the analog pin to read the output of the mux
+	pinMode(A0, INPUT);
+	// Setup headers for Excel import
+	Serial.print("A0,R0,A1,AR1,A2,AR2,A3,AR3,B0,BR0,B1,BR1,B2,BR2,B3,BR3,");
+	Serial.println("C0,CR0,C1,CR1,C2,CR2,C3,CR3");
+}
+
+void loop() {
+	// Turn on power to Plant A, B and D
+	digitalWrite(plantA, HIGH);
+	digitalWrite(plantB, HIGH);
+	digitalWrite(plantC, HIGH);
+
+	// Wait for charge to normalize
+	delay(45);
+	// Go through the pins and read their value
+	for( int i = 0; i < sensorNumber; i++ ){
+		A = bitRead(i,0);
+		B = bitRead(i,1);
+		C = bitRead(i,2);
+		D = bitRead(i,3);
+
+		// Write to multiplexer
+		digitalWrite(pinA, A);
+		digitalWrite(pinB, B);
+		digitalWrite(pinC, C);
+		digitalWrite(pinD, D);
+
+		// Read the analog pin
+		aPin = analogRead(A0);
+
+		// Write values to serial output
+		Serial.print(apin);
+		Serial.print(", ");
+
+		// Calculate resistance from arduino value
+		Serial.print(rA(aPin));
+
+		// Add another comma until the last calculation
+		if( i < (sensorNumber - 1)){
+			Serial.print(", ")
+		}
+	}
+
+	// End the line
+	Serial.println();
+
+	// Turn off power to the plants
+	digitalWrite(plantA, LOW);
+	digitalWrite(plantB, LOW);
+	digitalWrite(plantC, LOW);
+
+	// Wait for the specified time to run again
+	delay(wait);
+}
+
+double rA(double a){
+	return 0.1 * 1024 / a - 0.1;
 }
